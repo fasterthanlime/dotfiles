5669027c04253e15e7002b8d61551c5fd99ade28
Amos Wenger <amos@bearcove.net>
2024-05-28 23:50:01 +0200

    get rid of powerlevel10k

--- install.sh
+++ install.sh
@@ -4,13 +4,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 export ZSH="$HOME/.oh-my-zsh"
 
-if [[ -d $ZSH/custom/themes/powerlevel10k ]]; then
-  	echo "powerlevel10k is already installed"
-else
-	mkdir -p $ZSH/custom/themes
-	git clone https://github.com/romkatv/powerlevel10k.git $ZSH/custom/themes/powerlevel10k
-fi
-
 mkdir -p ~/.local/bin
 
 if [[ -d ~/.tmux/plugins/tpm ]]; then
@@ -24,7 +17,7 @@ basedir=$PWD
 
 mkdir -p ~/.config/
 
-for i in .p10k.zsh .zshrc .zshrc.d .tmux.conf .wezterm.lua .config/starship.toml .config/zed; do
+for i in .zshrc .zshrc.d .tmux.conf .wezterm.lua .config/starship.toml .config/zed; do
 	ln -f -s $PWD/$i ~/$(dirname $i)
 done
 echo "Creating symlinks... done!"
