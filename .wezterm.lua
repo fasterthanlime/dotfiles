28cf47c02af3c44324bfa4bdd1408d3245f1551b
Amos Wenger <amos@bearcove.net>
2024-08-12 21:20:12 +0200

    wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,21 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- Returns a bool based on whether the host operating system's
+-- appearance is light or dark.
+function is_dark()
+  -- wezterm.gui is not always available, depending on what
+  -- environment wezterm is operating in. Just return true
+  -- if it's not defined.
+  if wezterm.gui then
+    -- Some systems report appearance like "Dark High Contrast"
+    -- so let's just look for the string "Dark" and if we find
+    -- it assume appearance is dark.
+    return wezterm.gui.get_appearance():find("Dark")
+  end
+  return true
+end
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -113,13 +128,19 @@ end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 
-config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.window_decorations = 'RESIZE'
 config.win32_system_backdrop = 'Acrylic'
 if is_windows() then
     config.window_background_opacity = 0.73
 else
-    config.window_background_opacity = 1.0
+    config.window_background_opacity = 0.9
+    config.macos_window_background_blur = 30
 end
+
+config.window_frame = {
+  font = wezterm.font({ family = 'Berkeley Mono Variable', weight = 'Bold' }),
+  font_size = config.font_size
+}
 
 function trimString(str, len)
     if string.len(str) > len then
@@ -159,6 +180,67 @@ wezterm.on('format-tab-title', function(tab)
         title = formatDomain(pane.domain_name)
     end
     return title
+end)
+
+local function segments_for_right_status(window)
+  return {
+    window:active_workspace(),
+    wezterm.strftime('%a %b %-d %H:%M'),
+    wezterm.hostname(),
+  }
+end
+
+wezterm.on('update-status', function(window, _)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local segments = segments_for_right_status(window)
+
+  local color_scheme = window:effective_config().resolved_palette
+  -- Note the use of wezterm.color.parse here, this returns
+  -- a Color object, which comes with functionality for lightening
+  -- or darkening the colour (amongst other things).
+  local bg = wezterm.color.parse(color_scheme.background)
+  local fg = color_scheme.foreground
+
+  -- Each powerline segment is going to be coloured progressively
+  -- darker/lighter depending on whether we're on a dark/light colour
+  -- scheme. Let's establish the "from" and "to" bounds of our gradient.
+  local gradient_to, gradient_from = bg
+  if is_dark() then
+    gradient_from = gradient_to:lighten(0.2)
+  else
+    gradient_from = gradient_to:darken(0.2)
+  end
+
+  -- Yes, WezTerm supports creating gradients, because why not?! Although
+  -- they'd usually be used for setting high fidelity gradients on your terminal's
+  -- background, we'll use them here to give us a sample of the powerline segment
+  -- colours we need.
+  local gradient = wezterm.color.gradient(
+    {
+      orientation = 'Horizontal',
+      colors = { gradient_from, gradient_to },
+    },
+    #segments -- only gives us as many colours as we have segments.
+  )
+
+  -- We'll build up the elements to send to wezterm.format in this table.
+  local elements = {}
+
+  for i, seg in ipairs(segments) do
+    local is_first = i == 1
+
+    if is_first then
+      table.insert(elements, { Background = { Color = 'none' } })
+    end
+    table.insert(elements, { Foreground = { Color = gradient[i] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+    table.insert(elements, { Foreground = { Color = fg } })
+    table.insert(elements, { Background = { Color = gradient[i] } })
+    table.insert(elements, { Text = ' ' .. seg .. ' ' })
+  end
+
+  window:set_right_status(wezterm.format(elements))
 end)
 
 -- and finally, return the configuration to wezterm
